7982c0a47d606e384d198b89ffe9aea87844d756
Goran Admin <goran.admin@opkotor.loc>
2025-12-16 08:08:19 +0100

    Verifikacija maila

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url ?? config('app.url') }}" style="display: inline-block; color: #1f2937; font-weight: 600; font-size: 18px; text-decoration: none;">
+            {{ config('app.name', 'Digital Kotor') }}
+        </a>
+    </td>
+</tr>
+
